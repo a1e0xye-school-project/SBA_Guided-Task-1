0390b32ba963d707920f40adc9c5c66e46a5d004
A1e0xYe <github@mail.aye.moe>
2026-02-05 20:00:34 +0800

    feat: Player turns & Input validation & Cell replacing
    Detail change log:
    1. (feat) Empty cell could be define by change varaiable 'empty_cell_indicator'
    2. (feat) Determine which player turns by changing value of 'turn', default as player 1 first.
    3. (feat) Player inputted row and column validation, with range check whether within the board size.
    4. (feat) Check the valid inputtted row and column number cell is occupied or not, if yes, continue inputting the row and column number
    5. (style) Add anotation to specific lines for myself, and also report writing stage

--- main.py
+++ main.py
@@ -3,24 +3,48 @@
 
 # Varaiable
 turn = 0
+empty_cell_indicator = "*"
 
 board = []
-num_row_column = 15
+num_row_column = 15 # Board Size
 for i in range(num_row_column):
     board.append([])
     for r in range(num_row_column):
-        board[i].append("  ")
+        board[i].append(empty_cell_indicator)
 
 # Function
 ## Board Print
 def board_print(board):
     for i in range(num_row_column):
-        print(i, end="  ")
+        print(i, end=" ")
     print()
     for i in range(num_row_column):
-        print(i+1,end="  ")
+        print(i+1,end=" ")
         for r in range(num_row_column):
-            print(board[i][r], end="  ")
+            print(board[i][r], end=" ")
         print()
 
-board_print(board)
+
+
+# Main 
+while True:
+    board_print(board)
+    if turn == 0:
+        print("Player 1 turn")
+    else:
+        print("Player 2 turn")
+    while True:
+        selected_row = int(input("Enter the row: "))
+        selected_column = int(input("Enter the column: "))
+        if selected_row > 0 and selected_row <= num_row_column and selected_column > 0 and selected_column <= num_row_column:  # Data validation - within the board size
+            if board[selected_row - 1][selected_column - 1] == empty_cell_indicator:
+                if turn == 0:
+                    board[selected_row - 1][selected_column - 1] = "X"
+                else:
+                    board[selected_row - 1][selected_column - 1] = "O"
+                break  # Exit inner loop - input valid & finish replacing the cell
+            else:
+                print("This cell is already occupied") # Repeat loop - Input valid & Cell occupied
+        else:
+            print("Invalid input") # Repeat loop - Input invalid
+    turn = 1 - turn # Player round switch
