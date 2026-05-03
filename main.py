c5405cbc6b73d8962b4ffabed20827413a4491e8
A1e0xYe <github@mail.aye.moe>
2026-05-03 22:48:07 +0800

    feat: PvM game mode
    1. Added new game mode: PvM (Player vs Machine). Allowed player choose PvP or PvM mode. IF PvM, machine just aim to stop player winning this game. No change to origin PvP code (Human turn logic)

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # Gomoku Game 
 # 25-26 ICT SBA Guided Task 1
-# Library required: tabulate, termcolor
+# Library required: random, time, tabulate, termcolor
+# Second Edition: Add PvM mode
 
 # Dependencies
 try:
+    import random, time
     from tabulate import tabulate  # type: ignore
     from termcolor import colored, cprint  # type: ignore
 except ModuleNotFoundError as e:
@@ -115,33 +117,93 @@ while True:
 
     # Player input & Data validation
     while True:
-        while True:
-            selected_row = input(colored("Enter the row: ", "blue", attrs=["bold"]))
-            selected_column = input(colored("Enter the column: ", "blue", attrs=["bold"]))
-            if selected_row == '' or selected_column == '':   # Input nothing
-                cprint("You type nothing!!! Try again, please.", "red", attrs=["bold"])
-                continue    # Jump to next loop: Input again
-            selected_row = selected_row.strip() # Remove the whitespace
-            selected_column = selected_column.strip()
-            if selected_column.isdigit() and selected_row.isdigit():  # Check the strings only contain digit number
-                selected_column = int(selected_column)
-                selected_row = int(selected_row)
-                break
-            else:
-                cprint("Invalid input", "red", attrs=["bold"])
-                continue
+        if turn == 1 and game_mode == "PvM":        # PvM mode
+            # Machine turn logic
+            cprint("Machine is thinking...", "yellow", attrs=["bold"])
+                        
+            last_player_row_index = selected_row - 1
+            last_player_column_index = selected_column - 1
+            nearby_empty_cells = []
+            
+            # 1. Search nearby empty cells
+            for temp_row in range(-1, 2):
+                for temp_column in range(-1, 2):
+                    current_check_row = last_player_row_index + temp_row
+                    current_check_column = last_player_column_index + temp_column
+                    if 0 <= current_check_row < num_row_column:
+                        if 0 <= current_check_column < num_row_column:
+                            if board[current_check_row][current_check_column] == empty_cell_indicator:
+                                nearby_empty_cells.append((current_check_row, current_check_column))
 
-        if selected_row > 0 and selected_row <= num_row_column and selected_column > 0 and selected_column <= num_row_column:  # Data validation - within the board size
-            if board[selected_row - 1][selected_column - 1] == empty_cell_indicator:   # Data validation - EMPTY CELL 
-                if turn == 0:
-                    board[selected_row - 1][selected_column - 1] = player_1_indicator
+            pvm_machine_selected_move = None
+
+            # 2. Defense First
+            for empty_row, empty_column in nearby_empty_cells:
+                if check_win_v2(board, empty_row, empty_column, player_1_indicator):
+                    pvm_machine_selected_move = (empty_row, empty_column)
+                    break
+
+            # 3. Attack if no immediate threat
+            if not pvm_machine_selected_move:
+                for empty_row, empty_column in nearby_empty_cells:
+                    if check_win_v2(board, empty_row, empty_column, player_2_indicator):
+                        pvm_machine_selected_move = (empty_row, empty_column)
+                        break
+
+            # 4. FALLBACK: Random Move
+            if not pvm_machine_selected_move:
+                if len(nearby_empty_cells) > 0:
+                    pvm_machine_selected_move = random.choice(nearby_empty_cells) # Random select one nearby cell
+                else:  # IF: No empty cell nearby
+                    all_empty_on_board = []
+                    for row_index in range(num_row_column):  # Find all empty cells in the game board
+                        for column_index in range(num_row_column):
+                            if board[row_index][column_index] == empty_cell_indicator:
+                                all_empty_on_board.append((row_index, column_index))
+                    pvm_machine_selected_move = random.choice(all_empty_on_board)  # Random select one empty cell in whole game board
+
+            # 5. Apply machine selected move
+            board[pvm_machine_selected_move[0]][pvm_machine_selected_move[1]] = player_2_indicator
+            selected_row = pvm_machine_selected_move[0] + 1 
+            selected_column = pvm_machine_selected_move[1] + 1
+
+            time.sleep(1.5)   # Thinking time, just more realistic
+
+            cprint(f"Machine: I selected ({selected_row},{selected_column}).", "blue")
+
+            time.sleep(1.5)   # Machine selected result display
+
+            break # Exit input loop, continue to Check win part
+            
+        else:       # PvP mode
+            # Human turn logic
+            while True:
+                selected_row = input(colored("Enter the row: ", "blue", attrs=["bold"]))
+                selected_column = input(colored("Enter the column: ", "blue", attrs=["bold"]))
+                if selected_row == '' or selected_column == '':   # Input nothing
+                    cprint("You type nothing!!! Try again, please.", "red", attrs=["bold"])
+                    continue    # Jump to next loop: Input again
+                selected_row = selected_row.strip() # Remove the whitespace
+                selected_column = selected_column.strip()
+                if selected_column.isdigit() and selected_row.isdigit():  # Check the strings only contain digit number
+                    selected_column = int(selected_column)
+                    selected_row = int(selected_row)
+                    break
                 else:
-                    board[selected_row - 1][selected_column - 1] = player_2_indicator
-                break  # Exit inner loop - input valid & finish replacing the cell
+                    cprint("Invalid input", "red", attrs=["bold"])
+                    continue
+
+            if selected_row > 0 and selected_row <= num_row_column and selected_column > 0 and selected_column <= num_row_column:  # Data validation - within the board size
+                if board[selected_row - 1][selected_column - 1] == empty_cell_indicator:   # Data validation - EMPTY CELL 
+                    if turn == 0:
+                        board[selected_row - 1][selected_column - 1] = player_1_indicator
+                    else:
+                        board[selected_row - 1][selected_column - 1] = player_2_indicator
+                    break  # Exit inner loop - input valid & finish replacing the cell
+                else:
+                    cprint("This cell is already occupied", "red", attrs=["bold"]) # Repeat loop - Input valid & Cell occupied
             else:
-                cprint("This cell is already occupied", "red", attrs=["bold"]) # Repeat loop - Input valid & Cell occupied
-        else:
-            cprint("Invalid input", "red", attrs=["bold"]) # Repeat loop - Input invalid
+                cprint("Invalid input", "red", attrs=["bold"]) # Repeat loop - Input invalid
 
     # Check win
     if turn == 0:
